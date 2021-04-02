4da8ec04982700ef6cb5e2f3b76aa5d859683d3d
holmesr19 <holmesr19@mail.wlu.edu>
2021-04-01 22:48:52 -0400

    text->html

--- entries.js
+++ entries.js
@@ -3,8 +3,6 @@ const entryForm = "<div class='entry-form'>hello</div>";
 var entries = [];
 
 function addEntry() {
-    var entryDiv = document.getElementById('add-entries');
-    var text = document.createTextNode(entryForm);
-    entryDiv.appendChild(text);
+    document.getElementById('add-entries').innerHTML+=entryForm;
     
 }
