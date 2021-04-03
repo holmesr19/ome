38f111688af8bf170572de65b26103ec8d11b25e
holmesr19 <holmesr19@mail.wlu.edu>
2021-04-03 13:48:40 -0400

    nest entry logic into athlete

--- entries.js
+++ entries.js
@@ -1,12 +1,21 @@
-const entryForm = "<div class='entry-form'>entry form</div>";
-const child = "<div class='child-entries'>new child</div>";
+const entryForm = "<div class='entry-form'>\
+entry form\
+</div>";
+
+
+const athlete = "<div class='athlete-entries'>\
+new athlete\
+<div id = 'add-entries'></div>\
+    <button type='button' class='btn btn-secondary' onclick='addEntry()'>Add another entry</button>\
+</div>";
+
 
 function addEntry() {
     document.getElementById('add-entries').innerHTML+=entryForm;
 }
 
-function addChild() {
-    document.getElementById('add-child').innerHTML+=child;
+function addAthlete() {
+    document.getElementById('add-athlete').innerHTML+=athlete;
 }
 
 function submit() {
